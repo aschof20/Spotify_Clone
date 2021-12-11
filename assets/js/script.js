cf07060337b52c648732a4ae8bb9efff6ce63811
Alice Schofield <a.v.schofield@gmail.com>
2021-12-11 15:47:07 +1000

    Display length of song in playing now bar

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,34 @@ let currentPlaylist = Array()
 let audioElement;
 
 
+//Function to format the time.
+function formatTime(seconds) {
+    let time = Math.round(seconds);
+    let minutes = Math.floor(time / 60);
+    let second = time - minutes * 60;
+
+    // Account for string one digit seconds by adding leading 0.
+    if (second <= 9) {
+        second = "0" + second.toString()
+    } else {
+        second = second.toString()
+    }
+    return minutes + ":" + second;
+
+}
+
 function Audio() {
     this.currentlyPlaying;
     this.audio = document.createElement('audio');
+
+    // Update the time remaining in the playing bar.
+    this.audio.addEventListener("canplay", function () {
+        let duration = formatTime(this.duration)
+
+        $(".progressTime.remaining").text(duration);
+
+
+    })
 
     // Set the source of the audio file to play
     this.setTrack = function (track) {
